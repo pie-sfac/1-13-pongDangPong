38d2957d4c97d46fbf18a41162923d4e4bcdaec0
6suk <koandjo9116@gmail.com>
2023-07-19 18:14:15 +0900

    [⛏setting] absolute path 설정
    [23.07.19] 프로젝트 세팅 #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+import reactLogo from 'assets/react.svg';
+
+import viteLogo from '/vite.svg';
+
+import NewProject from 'components/NewProject';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <NewProject />
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
+          <img alt="Vite logo" className="logo" src={viteLogo} />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" rel="noreferrer" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -29,7 +33,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NewProject.tsx
@@ -0,0 +1,5 @@
+const NewProject = () => {
+  return <div>프로젝트 세팅 테스트 컴포넌트</div>;
+};
+
+export default NewProject;
