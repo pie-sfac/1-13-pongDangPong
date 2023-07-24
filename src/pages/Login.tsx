2f27a045d2d605da5528879551a9f8ba83f82135
6suk <koandjo9116@gmail.com>
2023-07-25 00:30:48 +0900

    [⚙️chore] 최적화 코드 lazy 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,47 @@
-import * as React from 'react';
-
+import { Modal, ModalValueState } from '@components/common/Modal';
 import { useAuth } from '@hooks/apis/useAuth';
 import useInput from '@hooks/utils/useInput';
+import { useModal } from '@hooks/utils/useModal';
 
 const initForm = {
   loginId: '',
   password: '',
 };
 
+const firstModalValues: ModalValueState = {
+  title: '일정 확인 필요',
+  desc: '취소를 진행하시겠습니까?\n취소는 차감된 횟수가 복구됩니다.\n주의 : 일정 내용 복구 불가',
+  button: [
+    {
+      id: 0,
+      title: '아니요',
+      color: false,
+    },
+    {
+      id: 1,
+      title: '예',
+      color: true,
+    },
+  ],
+};
+
+const secondModalValues: ModalValueState = {
+  title: '권한 없음',
+  desc: '삭제 권한이 없습니다.\n센터관리 > 직원관리에서 권한 설정을 변경해 주세요.',
+  button: [
+    {
+      id: 0,
+      title: '확인',
+      color: false,
+    },
+  ],
+};
+
 export const Login = () => {
   const { login, isLoading, authError } = useAuth();
   const [inputValues, handleInputChange, inputReset] = useInput(initForm);
+  const [isOpen, setIsOpen] = useModal();
+  const [isOpen_, setIsOpen_] = useModal();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -21,6 +52,10 @@ export const Login = () => {
 
   return (
     <>
+      <button onClick={() => setIsOpen(true)}>첫번째 모달 열기</button>
+      <Modal modalValues={firstModalValues} setIsOpen={setIsOpen} isOpen={isOpen} />
+      <button onClick={() => setIsOpen_(true)}>두번째 모달 열기</button>
+      <Modal modalValues={secondModalValues} setIsOpen={setIsOpen_} isOpen={isOpen_} />
       <h1>로그인</h1>
       {!isLoading ? (
         <form onSubmit={handleSubmit}>
@@ -47,3 +82,5 @@ export const Login = () => {
     </>
   );
 };
+
+export default Login;
